dce4dc5ff8bfdfb21561573db503752b036dd87a
misamisa-create <missmis0329@gmail.com>
2021-07-12 06:36:13 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e5a076e6ee0d845d43099d96fa5f83e1ad065bab9be5d6b049d707f84363845f04bf11fd698bf229e09c56b197323b6f5cdbebb2a9e9afb622ac9c41a89cddbb'
