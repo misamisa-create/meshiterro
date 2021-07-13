7b98e79f3676c9ca780d8dc661761422ec269f04
misamisa-create <missmis0329@gmail.com>
2021-07-13 03:00:20 +0000

    [Finish]4-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'homes#top'
   devise_for :users
-  resources:post_images,only:[:new,:create,:index,:show,:destroy]
+  root to: 'homes#top'
+  resources:post_images,only:[:new,:create,:index,:show,:destroy] do
+    resource:favorites,only:[:create,:destroy]
+    resources:post_comments,only:[:create,:destroy]
+  end
 end
